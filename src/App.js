6c619937e7c62d628e3bafda3a144744e6bc22f4
dev-from-cbd <veryfatwombat@gmail.com>
2024-01-07 08:29:36 +1030

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import { useEffect } from "react";
+import { Axios } from "axios";
 
 function App() {
-  return <div className="App">text</div>;
+  useEffect(() => {
+    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0").then(
+      (response) => {
+        setListOfCoins(response.data.coins);
+      }
+    );
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="cryptoHeader">
+        Cryptocurrency and Tokens Price Web App in React JS
+      </div>
+      <div className="cryptoDisplay">Div #2</div>
+    </div>
+  );
 }
 
 export default App;
