c94281ffa467f50381e3af08fed55747b46fcafb
dev-from-cbd <veryfatwombat@gmail.com>
2024-01-07 19:13:25 +1030

    Final update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
-import { useEffect } from "react";
-import { Axios } from "axios";
+import { useEffect, useState } from "react";
+import Axios from "axios";
+import Coin from "./components/Coin";
 
 function App() {
+  const [listOfCoins, setListOfCoins] = useState([]);
+  const [searchWord, setSearchWord] = useState("");
+
   useEffect(() => {
     Axios.get("https://api.coinstats.app/public/v1/coins?skip=0").then(
       (response) => {
@@ -11,12 +15,33 @@ function App() {
     );
   }, []);
 
+  const filteredCoins = listOfCoins.filter((coin) => {
+    return coin.name.toLowerCase().includes(searchWord.toLowerCase());
+  });
+
   return (
     <div className="App">
       <div className="cryptoHeader">
-        Cryptocurrency and Tokens Price Web App in React JS
+        <input
+          type="text"
+          placeholder="Bitcoin..."
+          onChange={(event) => {
+            setSearchWord(event.target.value);
+          }}
+        />
       </div>
-      <div className="cryptoDisplay">Div #2</div>
+      <div className="cryptoDisplay">
+        {filteredCoins.map((coin) => {
+          return (
+            <Coin
+              name={coin.name}
+              icon={coin.icon}
+              price={coin.price}
+              symbol={coin.symbol}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
